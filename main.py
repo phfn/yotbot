ac4dde172bef3fd07b0141e6787d227566801587
Paul Hoffmann <Paul.Hoffmann@novatec-gmbh.de>
2020-10-14 13:57:49 +0200

    First basically working version

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 import youtube_dl
 import sys
+import re
+import string
+import random
+import os
 
+
+def random_string(length=5):
+    ret=""
+    for i in range(length):
+        ret=ret+random.choice(string.ascii_letters)
+
+    return ret
 def start(update, context):
     update.message.reply_text(
         "Hi, nice to see you here. Just paste your links and i will download for uu")
 
 def dl(update, context):
+    path=random_string()
+    print(path)
     ytdl_argv = {'format': 'bestaudio/best',
                  'postprocessors': [{
                      'key': 'FFmpegExtractAudio',
                      'preferredcodec': 'mp3',
-                     'preferredquality': '320',
-                 }]
+                     'preferredquality': '192',
+                 }],
+                 "outtmpl": path+".webm"
                  }
-    with youtube_dl.YoutubeDL(ytdl_argv) as ytdl:
-        ytdl.download([update.message.text])
-
+    pat=re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))")
+    if pat.match(update.message.text):
+        with youtube_dl.YoutubeDL(ytdl_argv) as ytdl:
+            test=ytdl.download([update.message.text])
+        with open(path+".mp3", "rb") as file:
+            update.message.reply_audio(audio=file)
+        os.remove(path+".mp3")
+    else:
+        update.message.reply_text("Well that didnt work")
 token= sys.argv[1]
 updater = Updater(token, use_context=True)
-
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(filters.Filters.text, dl))
