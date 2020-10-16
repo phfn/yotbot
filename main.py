cfd73b382de3781154a753bb97d7fe3bcb2ab3e9
Paul Hoffmann <Paul.Hoffmann@novatec-gmbh.de>
2020-10-16 08:45:45 +0200

    Nutze nun die youtubesl main() statt der Embeddet version. Datei wird nach title benannt und enthält mp3 tags

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ import re
 import string
 import random
 import os
-from mutagen.mp3 import MP3
-from bs4 import BeautifulSoup, element, SoupStrainer
-import requests
-import urllib
-
-def get_video_title(url):
-    soup = BeautifulSoup(requests.get(url).text, features="html.parser", parse_only=SoupStrainer("title"))
+from mutagen.mp3 import MP3 as mp3_tags
 
 def random_string(length=5):
     ret=""
@@ -25,17 +19,16 @@ def start(update, context):
 def ytdl(url, preferredquality=320):
     path = random_string()
     print(path)
-    ytdl_argv = {'format': 'bestaudio/best',
-                 'postprocessors': [{
-                     'key': 'FFmpegExtractAudio',
-                     'preferredcodec': 'mp3',
-                     'preferredquality': str(preferredquality),
-                 }],
-                 "forcetitle": "",
-                 "outtmpl": path + ".webm"
-                 }
-    with youtube_dl.YoutubeDL(ytdl_argv) as ytdl:
-        ytdl.download([url])
+    youtube_dl.main(
+        ["--extract-audio",
+         "--audio-format", "mp3",
+         "--add-metadata",
+         "--embed-thumbnail",
+         "--audio-quality", str(preferredquality),
+         "-o", path + ".webm",
+         url])
+    print("test")
+
     return path
 
 
@@ -44,20 +37,31 @@ def dl(update, context):
     pat = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))")
     if pat.match(update.message.text):
         path=ytdl(update.message.text)
+        print("ytdl is fertig")
+
+        #geningere Bitrate nutzen, wenn Datei zu groß wäre
         if os.stat(path + ".mp3").st_size > 50_000_000:
             path = ytdl(update.message.text, 192)
             if os.stat(path + ".mp3").st_size > 50_000_000:
                 path = ytdl(update.message.text, 128)
                 if os.stat(path + ".mp3").st_size > 50_000_000:
                     update.message.reply_text("Die Datei war leider zu Groß")
-        print(os.stat(path+".mp3").st_size)
 
+        #rename File based on the title tag
+        new_path=str(mp3_tags(path+".mp3")["TIT2"])
+        os.rename(path+".mp3", new_path+".mp3")
+        path=new_path
+
+        #datei senden
         with open(path+".mp3", "rb") as file:
             update.message.reply_audio(audio=file)
         os.remove(path+".mp3")
     else:
         update.message.reply_text("Well that didnt work")
     print("finisch")
+
+
+
 token= sys.argv[1]
 updater = Updater(token, use_context=True)
 
